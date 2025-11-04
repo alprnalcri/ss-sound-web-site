1e3ebd3be6e4cfc7c5c71fb4dfeee69d025599fd
alprnalcri <a.alaceri@outlook.com>
2025-11-04 14:23:19 +0300

    Fix: path-to-regexp error by changing wildcard route

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
   // API rotaları dışındaki tüm istekleri client'ın index.html'ine yönlendir
-  app.get('/*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 }
